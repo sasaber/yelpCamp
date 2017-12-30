f177349e0972c0472ba40f35e5974f50f309e38f
sasaber <s.ahmsab@gmail.com>
2017-12-30 00:24:30 -0800

    added database

--- app.js
+++ app.js
@@ -1,43 +1,89 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser = require("body-parser"),
+	mongoose 	= require("mongoose");
 
-var campgrounds = [	
-	{name: "Bola2", image:"https://images.unsplash.com/photo-1486082570281-d942af5c39b7?auto=format&fit=crop&w=1502&q=80"},
-	{name: "Sa2ara", image: "https://images.unsplash.com/photo-1496545672447-f699b503d270?auto=format&fit=crop&w=1502&q=80"},
-	{name: "Faisal", image: "https://images.unsplash.com/photo-1478810810369-07072c5ef88b?auto=format&fit=crop&w=1500&q=80"},
-	{name: "Bola2", image:"https://images.unsplash.com/photo-1486082570281-d942af5c39b7?auto=format&fit=crop&w=1502&q=80"},
-	{name: "Sa2ara", image: "https://images.unsplash.com/photo-1496545672447-f699b503d270?auto=format&fit=crop&w=1502&q=80"},
-	{name: "Faisal", image: "https://images.unsplash.com/photo-1478810810369-07072c5ef88b?auto=format&fit=crop&w=1500&q=80"},
-	{name: "Bola2", image:"https://images.unsplash.com/photo-1486082570281-d942af5c39b7?auto=format&fit=crop&w=1502&q=80"},
-	{name: "Sa2ara", image: "https://images.unsplash.com/photo-1496545672447-f699b503d270?auto=format&fit=crop&w=1502&q=80"},
-	{name: "Faisal", image: "https://images.unsplash.com/photo-1478810810369-07072c5ef88b?auto=format&fit=crop&w=1500&q=80"}
-];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// schema setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+/*
+Campground.create(
+	{
+		name: "Granite Hill",
+		image: "https://images.unsplash.com/photo-1439123414876-0717652a92a9?auto=format&fit=crop&w=1504&q=80",
+		description: "This a huge granite hill. Absolutely beautiful. No water, no bathrooms."
+	}, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(newlyCreated);
+		}
+});
+*/
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds: campgrounds});
-});
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {campgrounds: allCampgrounds});
+		}
+	});
+/*	res.render("campgrounds", {campgrounds: campgrounds});
+*/});
 
+// CREATE - add new campground to db
 app.post("/campgrounds", function(req, res){
 	// get data from form and add to campground array
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	// redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
+	// Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
 	res.render("new");
 });
+
+// SHOW - shows info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	// find the campground with provided ID
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			// render show template with that campground
+			res.render("show", {campground: foundCampground});
+		}
+	});
+});
+
 
 app.listen(3000, function(){
 	console.log("The YelpCamp Server has Started");
